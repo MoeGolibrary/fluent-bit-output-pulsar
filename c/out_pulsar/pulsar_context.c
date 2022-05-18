46861a82c293c69bdc8c5f81a68ffde5658562e7
FrankXiong <Frank_Xiong_@outlook.com>
2022-05-19 00:24:13 +0800

    update log

--- c/out_pulsar/pulsar_context.c
+++ c/out_pulsar/pulsar_context.c
@@ -1,9 +1,5 @@
 
 #include <fluent-bit/flb_output_plugin.h>
-//#include <fluent-bit/flb_utils.h>
-//#include <fluent-bit/flb_pack.h>
-//#include <fluent-bit/flb_sds.h>
-//#include <fluent-bit/flb_kv.h>
 
 #include <pulsar/c/authentication.h>
 #include <pulsar/c/client.h>
@@ -11,18 +7,34 @@
 
 #define PULSAR_DEFAULT_MEMORY_LIMIT 1024
 
+char* append_log_text(char *config, const char *key, const char* value)
+{
+    char *start = config;
+    while (*key) {
+        *config++ = *key++;
+    }
+    while (*value) {
+        *config++ = *value++;
+    }
+    *config++ = '\n';
+
+    return config;
+}
+
 flb_out_pulsar_ctx* flb_out_pulsar_create(const struct flb_output_instance *ins, const struct flb_config* config)
 {
     int ret;
-    long memory_limit;
-    long send_timeout;
-    long batch_max_msg;
-    long batch_max_bytes;
-    long batch_max_delay;
-    long max_pending_msg;
-    long max_pending_par;
-    const char *pvalue;
     pulsar_result err;
+    const char *pvalue;
+    long memory_limit = 0;
+    long send_timeout = 0;
+    long batch_max_msg = 0;
+    long batch_max_bytes = 0;
+    long batch_max_delay = 0;
+    long max_pending_msg = 0;
+    long max_pending_par = 0;
+    char config_log[1 << 12] = { 0 };
+    char *plog = config_log;
 
     flb_out_pulsar_ctx *ctx = flb_calloc(1, sizeof(flb_out_pulsar_ctx));
     if (!ctx) {
@@ -46,7 +58,7 @@ flb_out_pulsar_ctx* flb_out_pulsar_create(const struct flb_output_instance *ins,
     ctx->success_number = 0;
     ctx->show_interval = DEFAULT_SHOW_INTERVAL;
 
-    // loca config
+    // load config
     ret = flb_output_config_map_set(ins, (void*) ctx);
     if (ret < 0) {
         flb_free(ctx);
@@ -64,6 +76,9 @@ flb_out_pulsar_ctx* flb_out_pulsar_create(const struct flb_output_instance *ins,
     ctx->url_len = strlen(ctx->url);
     ctx->topic_len = strlen(ctx->topic);
 
+    plog = append_log_text(plog, "init pulsar ok !!!", " config:");
+    plog = append_log_text(plog, "    PulsarUrl:                          ", ctx->url);
+
     /*
      * config and create pulsar client
      */
@@ -72,11 +87,14 @@ flb_out_pulsar_ctx* flb_out_pulsar_create(const struct flb_output_instance *ins,
         ctx->token_len = strlen(ctx->token);
         ctx->authentication = pulsar_authentication_token_create(ctx->token);
         pulsar_client_configuration_set_auth(ctx->conf, authentication);
+        plog = append_log_text(plog, "    Token:                              ", ctx->token);
     }
+
 
     pvalue = flb_output_get_property("MemoryLimit", ins);
     if (pvalue && PULSAR_DEFAULT_MEMORY_LIMIT < (memory_limit = atol(pvalue))) {
         pulsar_client_configuration_set_memory_limit(ctx->conf, memory_limit);
+        plog = append_log_text(plog, "    MemoryLimit:                        ", pvalue);
     }
     
     ctx->client = pulsar_client_create(pulsar_url, ctx->conf);
@@ -95,8 +113,11 @@ flb_out_pulsar_ctx* flb_out_pulsar_create(const struct flb_output_instance *ins,
     pvalue = flb_output_get_property("ProducerName", ins);
     if (pvalue) {
         pulsar_producer_configuration_set_producer_name(ctx->producer_conf, pvalue);
+        plog = append_log_text(plog, "    ProducerName:                       ", pvalue);
     }
     
+    plog = append_log_text(plog, "    Topic:                              ", pvalue);
+
     // CompressType
     pvalue = flb_output_get_property("CompressType", ins);
     if (pvalue) {
@@ -113,12 +134,14 @@ flb_out_pulsar_ctx* flb_out_pulsar_create(const struct flb_output_instance *ins,
         } else {
             flb_plg_warn(ins, "unsupported pulsar compress type: %s", pvalue);
         }
+        plog = append_log_text(plog, "    CompressType:                       ", pvalue);
     }
 
     // SendTimeout
     pvalue = flb_output_get_property("SendTimeout", ins);
     if (pvalue && 0 < (send_timeout = atol(pvalue))) {
         pulsar_producer_configuration_set_send_timeout(ctx->producer_conf, send_timeout);
+        plog = append_log_text(plog, "    SendTimeout:                        ", pvalue);
     }
 
     // Batching config
@@ -129,18 +152,22 @@ flb_out_pulsar_ctx* flb_out_pulsar_create(const struct flb_output_instance *ins,
         } else if (0 == strcasecmp("false", pvalue)) {
             pulsar_producer_configuration_set_batching_enabled(ctx->producer_conf, false);
         }
+        plog = append_log_text(plog, "    BatchingEnabled:                    ", pvalue);
     }
     pvalue = flb_output_get_property("BatchingMaxMessages", ins);
     if (pvalue && 0 < (batch_max_msg = atol(pvalue))) {
         pulsar_producer_configuration_set_batching_max_messages(ctx->producer_conf, batch_max_msg);
+        plog = append_log_text(plog, "    BatchingMaxMessages:                ", pvalue);
     }
     pvalue = flb_output_get_property("BatchingMaxBytes", ins);
     if (pvalue && 0 < (batch_max_bytes = atol(pvalue))) {
         pulsar_producer_configuration_set_batching_max_allowed_size_in_bytes(ctx->producer_conf, batch_max_bytes);
+        plog = append_log_text(plog, "    BatchingMaxBytes:                   ", pvalue);
     }
     pvalue = flb_output_get_property("BatchingMaxPublishDelay", ins);
     if (pvalue && 0 < (batch_max_delay = atol(pvalue))) {
         pulsar_producer_configuration_set_batching_max_publish_delay_ms(ctx->producer_conf, batch_max_delay);
+        plog = append_log_text(plog, "    BatchingMaxPublishDelay:            ", pvalue);
     }
 
     // BlockIfQueueFull
@@ -151,18 +178,21 @@ flb_out_pulsar_ctx* flb_out_pulsar_create(const struct flb_output_instance *ins,
         } else if (0 == strcasecmp("false", pvalue)) {
             pulsar_producer_configuration_set_block_if_queue_full(ctx->producer_conf, false);
         }
+        plog = append_log_text(plog, "    BlockIfQueueFull:                   ", pvalue);
     }
 
     // MaxPendingMessages
     pvalue = flb_output_get_property("MaxPendingMessages", ins);
     if (pvalue && 0 < (max_pending_msg = atol(pvalue))) {
         pulsar_producer_configuration_set_max_pending_messages(ctx->producer_conf, max_pending_msg);
+        plog = append_log_text(plog, "    MaxPendingMessages:                 ", pvalue);
     }
 
     // MaxPendingMessagesAcrossPartitions
     pvalue = flb_output_get_property("MaxPendingMessagesAcrossPartitions", ins);
     if (pvalue && 0 < (max_pending_par = atol(pvalue))) {
         pulsar_producer_configuration_set_max_pending_messages_across_partitions(ctx->producer_conf, max_pending_par);
+        plog = append_log_text(plog, "    MaxPendingMessagesAcrossPartitions: ", pvalue);
     }
 
     // create pulsar producer
@@ -172,7 +202,8 @@ flb_out_pulsar_ctx* flb_out_pulsar_create(const struct flb_output_instance *ins,
         flb_plg_error(ins, "Failed to create pulsar producer: %s\n", pulsar_result_str(err));
         return NULL;
     }
-
+   
+    flb_plg_info(ins, config_log);
     return ctx;
 }
 
